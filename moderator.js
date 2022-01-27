96d728db3e63a72567ed8652846db512b37cf83f
Codycody31 <Github@codycody.org>
2022-01-27 10:07:47 -0500

    Update moderator.js

--- moderator.js
+++ moderator.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const { embed } = require('discord.js'); 
 const { Message } = require('discord.js');
 const client = new Discord.Client({partials: ["MESSAGE", "GUILDS", "GUILD_MESSAGES"]})
-const noNoWords = ["sex", "condum", "bad", "fuck", "rape", "shit", "FU", ".me/"];
+const noNoWords = ["sex", "condum", "bad", "fuck", "rape", "shit", ".me/"];
+const summon = ["Troop", "333", "Troop 333"]
 client.login(process.env.BOT_TOKEN);
 client.on("message", message => {
     var content = message.content;
@@ -12,6 +13,14 @@ client.on("message", message => {
 			.then(() => console.log(`The moderator Bot removed the word ${message.content} by ${message.author.username} from the channel ${message.channel.name}`))
 			.catch(console.error)
 			message.channel.send("Uh-ho! <@" + message.author + "> you said a no-no word!");
+            break
+        }
+    }
+    for (var i = 0; i < summon.length; i++) {
+        if (content.toLowerCase().includes(summon[i])){  
+            message.channel.send("<@" + message.author + "> you summoned me!")
+			.then(() => console.log(`The moderator Bot was summoned by ${message.author.username} from the channel ${message.channel.name}`))
+			.catch(console.error);
             break
         }
     }

8fe94ce1c06670fe2d670d60a296890b883a8898
Codycody31 <Github@codycody.org>
2022-01-27 09:45:36 -0500

    Update moderator.js

--- moderator.js
+++ moderator.js
@@ -7,7 +7,7 @@ client.login(process.env.BOT_TOKEN);
 client.on("message", message => {
     var content = message.content;
     for (var i = 0; i < noNoWords.length; i++) {
-        if (content.toLowerCase.includes(noNoWords[i])){  
+        if (content.toLowerCase().includes(noNoWords[i])){  
             message.delete()
 			.then(() => console.log(`The moderator Bot removed the word ${message.content} by ${message.author.username} from the channel ${message.channel.name}`))
 			.catch(console.error)

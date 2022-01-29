a1a8be705d4bbf2aaac46d94056639a5640cf3d5
Codycody31 <Github@codycody.org>
2022-01-28 23:30:05 -0500

    Update moderator.js

--- moderator.js
+++ moderator.js
@@ -3,7 +3,7 @@ const { embed } = require('discord.js');
 const { Message } = require('discord.js');
 const client = new Discord.Client({partials: ["MESSAGE", "GUILDS", "GUILD_MESSAGES"]})
 const noNoWords = ["sex", "condum", "bad", "fuck", "rape", "shit", "nude"];
-const scam = [".me/", "discord.gifts/billing/promotion/", "discord-airdrop.fun/nitro", "discord-niltro.com/"];
+const scam = [".me/", "discord.gifts/billing/promotion/", "discord-airdrop.fun/nitro", "discord-niltro.com/", "https://discrde.gift/QEHnDhAbECvcW"];
 client.login(process.env.BOT_TOKEN);
 client.on("message", message => {
     var content = message.content;
@@ -17,12 +17,17 @@ client.on("message", message => {
         }
     }
     for (var i = 0; i < scam.length; i++) {
-        if (content.toLowerCase().includes(scam[i])){  
-            message.delete()
-			.then(() => console.log(`The moderator Bot removed the message "${message.content}" by ${message.author.username} from the channel ${message.channel.name}`))
-			.catch(console.error)
-			message.channel.send("Uh-ho! <@" + message.author + "> you tried to send a link for discord! Due to the usage of this link for scam messages the sending of this type of link has been banned!");
-            break
+        if (content.toLowerCase().includes(scam[i])){
+            if (message.member.roles.cache.some(role => role.name === 'troop leaders')){
+                break
+            }
+            else{
+                message.delete()
+                .then(() => console.log(`The moderator Bot removed the message "${message.content}" by ${message.author.username} from the channel ${message.channel.name}`))
+                .catch(console.error)
+                message.channel.send("Uh-ho! <@" + message.author + "> you tried to send a link for discord! Due to the usage of this link for scam messages the sending of this type of link has been banned!");
+                break
+            }
         }
     }
 })
